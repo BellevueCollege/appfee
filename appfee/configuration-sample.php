f1a90906755a54b3f05257fb4f1418840325859d
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-10 11:29:47 -0700

    Add configuration-sample.php File

---
+++ appfee/configuration-sample.php
@@ -0,0 +1,25 @@
+<?php
+
+define( 'BASE_URI', '' );
+
+define( 'CURRENCY', '' );
+
+define( 'CYBERSOURCE_ACCESS_KEY', '' );
+
+define( 'CYBERSOURCE_LOCALE', '' );
+
+define( 'CYBERSOURCE_PROFILE_ID', '' );
+
+define( 'CYBERSOURCE_SECRET_KEY', '' );
+
+define( 'DEFAULT_BILL_TO_ADDRESS_COUNTRY', '' );
+
+define( 'DEFAULT_BILL_TO_ADDRESS_STATE', '' );
+
+define( 'DEFAULT_TRANSACTION_TYPE', '' );
+
+define( 'FORM_POST_URL', '' );
+
+define( 'GLOBALS_PATH', '' );
+
+define( 'GLOBALS_URL', '' );
