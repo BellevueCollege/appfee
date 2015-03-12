806ecc041aaf3cde9c50a8cfe90c841f59887f0a
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-12 16:20:18 -0700

    Add Additional Functionality to the Default_Model
    Add the following functionality:
    
    * Support for tracking errors with an error array and helper methods
    * Redirect URL variable with helper functions
    * New get_current_url method that returns the current page URL
    * New helper function to set the the template_uri variable

--- appfee/model/default-model.php
+++ appfee/model/default-model.php
@@ -5,6 +5,8 @@
  */
 class Default_Model {
 
+	public $errors;
+	public $redirect_url;
 	public $globals_path;
 	public $globals_url;
 	public $template_uri;
@@ -19,6 +21,26 @@ class Default_Model {
 		if ( isset( $globals_url ) ) {
 			$this->globals_url = $globals_url;
 		}
+		$this->errors = array();
+	}
+
+	public function add_error( $error_message ) {
+		$this->errors[] = $error_message;
+	}
+
+	public function get_current_url() {
+		$request_scheme = ( isset( $_SERVER['HTTPS'] ) ) ? 'https' : 'http';
+		$current_url =
+			$request_scheme .
+			'://' .
+			$_SERVER['HTTP_HOST'] .
+			$_SERVER['REQUEST_URI']
+		;
+		return $current_url;
+	}
+
+	public function get_errors() {
+		return $this->errors;
 	}
 
 	public function get_globals_path() {
@@ -29,7 +51,19 @@ class Default_Model {
 		return $this->globals_url;
 	}
 
+	public function get_redirect_url() {
+		return $this->redirect_url;
+	}
+
 	public function get_template_uri() {
 		return $this->template_uri;
+	}
+
+	public function set_redirect_url( $url ) {
+		$this->redirect_url = $url;
+	}
+
+	public function set_template_uri( $uri ) {
+		$this->template_uri = $uri;
 	}
 }
