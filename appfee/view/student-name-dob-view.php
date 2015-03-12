a51d5d80989ae8670a8296a1cda7b17a2f89551a
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-12 16:38:09 -0700

    Update Student_Name_DOB_View to use new Default_Model

--- appfee/view/student-name-dob-view.php
+++ appfee/view/student-name-dob-view.php
@@ -8,10 +8,12 @@ require_once( 'default-view.php' );
 class Student_Name_DOB_View extends Default_View {
 
 	public function get_output() {
+		$current_url = $this->model->get_current_url();
+		$errors = $this->model->get_errors();
+		$form_post_url = $this->model->get_form_post_url();
 		$globals_path = $this->model->get_globals_path();
 		$globals_url  = $this->model->get_globals_url();
 		$template_uri = $this->model->get_template_uri();
-		$form_post_url = $this->model->get_form_post_url();
 		require_once( $template_uri );
 	}
 }
