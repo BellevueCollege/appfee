8b4778a4249e91af744a6b64d5cd012426e1cdfe
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-12 16:40:30 -0700

    Update Cybersource_Payment_View to use new Default_Model

--- appfee/view/cybersource-payment-view.php
+++ appfee/view/cybersource-payment-view.php
@@ -11,9 +11,11 @@ class Cybersource_Payment_View extends Default_View {
 		$bill_to_address_country = $this->model->get_bill_to_address_country();
 		$bill_to_address_state = $this->model->get_bill_to_address_state();
 		$currency = $this->model->get_currency();
+		$current_url = $this->model->get_current_url();
 		$cybersource_access_key = $this->model->get_cybersource_access_key();
 		$cybersource_locale = $this->model->get_cybersource_locale();
 		$cybersource_profile_id = $this->model->get_cybersource_profile_id();
+		$errors = $this->model->get_errors();
 		$form_post_url = $this->model->get_form_post_url();
 		$globals_path = $this->model->get_globals_path();
 		$globals_url  = $this->model->get_globals_url();
@@ -22,6 +24,7 @@ class Cybersource_Payment_View extends Default_View {
 		$item_0_unit_price = $this->model->get_item_0_unit_price();
 		$line_item_count = $this->model->get_line_item_count();
 		$reference_number = $this->model->get_reference_number();
+		$redirect_url = $this->model->get_redirect_url();
 		$signature = $this->model->get_signature();
 		$signed_date_time = $this->model->get_signed_date_time();
 		$signed_field_names = $this->model->get_signed_field_names();
