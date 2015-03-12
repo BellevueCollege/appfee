17d5c3c4b5a5a22d7339b019b64a08ed871cf545
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-12 16:42:01 -0700

    Add Error Handling to Cybersource_Payment_Controller
    Add the following error handling logic to the
    Cybersource_Payment_Controller class:
    
    * New regular expressions to handle date of birth and name validation
    * New methods is_valid_date_of_birth and is_valid_name to help with
      validation
    * Confirm all required form fields were passed to the controller before
      processing begins
    * Set view to respond with blank page if all expected fields were not
      passed
    * Record errors on invalid form fields
    * Set view to respond with a redirect if invalid form fields were
      detected
    * Expect that the form will pass a new field called form_url so that we
      know where to redirect if an error has been detected

--- appfee/controller/cybersource-payment-controller.php
+++ appfee/controller/cybersource-payment-controller.php
@@ -8,22 +8,120 @@ require_once( 'default-controller.php' );
 
 class Cybersource_Payment_Controller extends Default_Controller {
 
+	public $regex_date_of_birth;
+	public $regex_name;
+
+	public function __construct( $model ) {
+		parent::__construct( $model );
+
+		$this->regex_date_of_birth =
+			'/^(0?[1-9]|1[0-2])\/' .
+			'(0?[1-9]|[1-2][0-9]|3[0-1])\/' .
+			'((19|[2-9][0-9])[0-9]{2})$/'
+		;
+		$this->regex_name = '/^[a-z\-.,()\'"\s]+$/i';
+	}
+
+	public function is_valid_date_of_birth( $date_of_birth ) {
+		$is_valid_date = preg_match(
+			$this->regex_date_of_birth,
+			$date_of_birth
+		);
+		if ( ! $is_valid_date ) {
+			return false;
+		}
+
+		$date_of_birth_integer = strtotime( $date_of_birth );
+		if ( $date_of_birth_integer > time() ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public function is_valid_name( $name ) {
+		$is_valid_name = preg_match(
+			$this->regex_name,
+			$name
+		);
+		if ( ! $is_valid_name ) {
+			return false;
+		}
+		return true;
+	}
+
 	public function save_data( $post_array ) {
-		$date_of_birth = $post_array['date_of_birth'];
-		$first_name = $post_array['first_name'];
-		$last_name = $post_array['last_name'];
+		// Set initial variables
+		$data_not_valid = false;
+
+		/*
+		 * Check that all required form fields were posted. On failure load a
+		 * blank page because someone is likely tampering with the form.
+		 */
+		if ( isset( $post_array['form_url'] )
+			&& isset( $post_array['date_of_birth'] )
+			&& isset( $post_array['first_name'] )
+			&& isset( $post_array['last_name'] )
+		) {
+			$form_url = $post_array['form_url'];
+			$date_of_birth = $post_array['date_of_birth'];
+			$first_name = $post_array['first_name'];
+			$last_name = $post_array['last_name'];
+		} else {
+			$this->model->set_template_uri(
+				'template/blank-page-template.php'
+			);
+		}
+
 		$middle_name = NULL;
 		if ( isset( $post_array['middle_name'] ) ) {
 			$middle_name = $post_array['middle_name'];
 		}
 
+		// Validate variables
+		if ( ! $this->is_valid_date_of_birth( $date_of_birth ) ) {
+			$this->model->add_error(
+				'Date of birth is not valid or is in the future.'
+			);
+			$data_not_valid = true;
+		}
+		if ( ! $this->is_valid_name( $first_name ) ) {
+			$this->model->add_error(
+				'First name contains invalid characters.'
+			);
+			$data_not_valid = true;
+		}
+		if ( ! $this->is_valid_name( $last_name ) ) {
+			$this->model->add_error(
+				'Last name contains invalid characters.'
+			);
+			$data_not_valid = true;
+		}
+		if ( isset( $middle_name ) && ! $this->is_valid_name( $middle_name ) ) {
+			$this->model->add_error(
+				'Middle name contains invalid characters.'
+			);
+			$data_not_valid = true;
+		}
+
+		// If variables are not valid
+		if ( $data_not_valid ) {
+			$this->model->set_redirect_url( $form_url );
+			$this->model->set_template_uri(
+				'template/error-redirect-template.php'
+			);
+			return true;
+		}
+
+		// Set model variables
 		$this->model->set_student_date_of_birth( $date_of_birth );
 		$this->model->set_student_first_name( $first_name );
 		$this->model->set_student_last_name( $last_name );
 		$this->model->set_student_middle_name( $middle_name );
 
+		// Have model save the data
 		$this->model->save_data();
 
-		return True;
+		return true;
 	}
 }
