9501fbe50a9aad0f5e28a9f4c23a5141df974f0a
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-03-18 12:30:36 -0700

    Collect Program of Study in Cybersource_Payment_Template

--- appfee/controller/cybersource-payment-controller.php
+++ appfee/controller/cybersource-payment-controller.php
@@ -8,9 +8,26 @@ require_once( 'name-dob-controller.php' );
 
 class Cybersource_Payment_Controller extends Name_DOB_Controller {
 
+	public function get_program_of_study( $post_array ) {
+		if ( ! empty( $post_array['proftech_type'] ) ) {
+			return $post_array['proftech_type'];
+		} elseif ( ! empty( $post_array['what_degree'] ) ) {
+			return $post_array['what_degree'];
+		} elseif ( ! empty( $post_array['no_degree'] ) ) {
+			return $post_array['no_degree'];
+		} else {
+			return false;
+		}
+	}
+
+	public function is_valid_program_of_study( $program_of_study ) {
+		return true;
+	}
+
 	public function save_data( $post_array ) {
 		$data_not_valid = false;
 		$honey_pot_field = 'no_fill';
+		$program_of_study = $this->get_program_of_study( $post_array );
 
 		/*
 		 * Check that all required form fields were posted. On failure load a
@@ -21,6 +38,7 @@ class Cybersource_Payment_Controller extends Name_DOB_Controller {
 			&& ! empty( $post_array['date_of_birth'] )
 			&& ! empty( $post_array['first_name'] )
 			&& ! empty( $post_array['last_name'] )
+			&& $program_of_study
 			&& ( array_key_exists( $honey_pot_field, $post_array )
 				&& empty( $post_array[ $honey_pot_field ] )
 			)
@@ -60,6 +78,11 @@ class Cybersource_Payment_Controller extends Name_DOB_Controller {
 			);
 			$data_not_valid = true;
 		}
+		if ( ! $this->is_valid_program_of_study( $program_of_study ) ) {
+			$this->model->add_error(
+				'The program of study code is invalid.'
+			);
+		}
 		if ( isset( $middle_name ) && ! $this->is_valid_name( $middle_name ) ) {
 			$this->model->add_error(
 				'Middle name contains invalid characters.'
@@ -81,6 +104,7 @@ class Cybersource_Payment_Controller extends Name_DOB_Controller {
 		$this->model->set_student_first_name( $first_name );
 		$this->model->set_student_last_name( $last_name );
 		$this->model->set_student_middle_name( $middle_name );
+		$this->model->set_program_of_study( $program_of_study );
 
 		// Have model save the data
 		$this->model->save_data();
