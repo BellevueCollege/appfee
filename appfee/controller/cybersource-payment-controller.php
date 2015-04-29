49ea00009f50260c28b8be238b93a1a133337e4f
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2015-04-29 10:39:20 -0700

    Refactor CyberSource Payment Controller

--- appfee/controller/cybersource-payment-controller.php
+++ appfee/controller/cybersource-payment-controller.php
@@ -52,30 +52,59 @@ class Cybersource_Payment_Controller extends Name_DOB_Controller {
 	 * Get the program of study code from the post array.
 	 *
 	 * @since 1.0.0
-	 *
-	 * @param array $args {
-	 *     Array of key values usually passed in from the PHP $_POST variable.
-	 *
-	 *     @type string $proftech_type Optional. Professional technical
-	 *                                 certificate / degree code.
-	 *     @type string $what_degree   Optional. A non-professional technical
-	 *                                 certificate / degree code.
-	 *     @type string $no_degree     Optional. A code for non-degree
-	 *                                 selections.
-	 * }
 	 * @return string|false Program of study code or false if program of study
 	 *                      can not be found.
 	 */
-	public function get_program_of_study( $post_array ) {
-		if ( ! empty( $post_array['proftech_type'] ) ) {
-			return $post_array['proftech_type'];
-		} elseif ( ! empty( $post_array['what_degree'] ) ) {
-			return $post_array['what_degree'];
-		} elseif ( ! empty( $post_array['no_degree'] ) ) {
-			return $post_array['no_degree'];
+	public function get_program_of_study_code() {
+		if ( ! empty( $this->data['proftech_type'] ) ) {
+			return $this->data['proftech_type'];
+		} elseif ( ! empty( $this->data['what_degree'] ) ) {
+			return $this->data['what_degree'];
+		} elseif ( ! empty( $this->data['no_degree'] ) ) {
+			return $this->data['no_degree'];
 		} else {
 			return false;
 		}
+	}
+
+	/**
+	 * Validate data property values.
+	 *
+	 * Validates that all key values are valid in the data array.
+	 *
+	 * @since 1.0.0
+	 * @see Name_DOB_Controller::$data
+	 * @return boolean True if data is valid.
+	 */
+	public function is_valid_data() {
+		$data_valid = parent::is_valid_data();
+		if ( ! $this->is_valid_program_of_study(
+				$this->get_program_of_study_code()
+			)
+		) {
+			$this->model->add_error(
+				'The program of study code is invalid.'
+			);
+			$data_valid = false;
+		}
+		return $data_valid;
+	}
+
+	/**
+	 * Validate data property keys.
+	 *
+	 * Validates that all required keys exsist in the data array.
+	 *
+	 * @since 1.0.0
+	 * @see Name_DOB_Controller::$data
+	 * @return boolean True if keys are valid.
+	 */
+	public function is_valid_keys() {
+		$parent_keys_valid = parent::is_valid_keys();
+		if ( $parent_keys_valid && $this->get_program_of_study_code() ) {
+			return true;
+		}
+		return false;
 	}
 
 	// TODO: Implement program of study code validation.
@@ -84,134 +113,18 @@ class Cybersource_Payment_Controller extends Name_DOB_Controller {
 	}
 
 	/**
-	 * Validate and save HTTP post data to the model object.
+	 * Save data variables to model object.
 	 *
-	 * Validates an array of key values (usually from HTTP post) and saves
-	 * values from the array to a model object.
+	 * Helper function that saves data to the model object.
 	 *
 	 * @since 1.0.0
-	 *
-	 * @param array $args {
-	 *     Array of key values usually passed in from the PHP $_POST variable.
-	 *
-	 *     @type string $date_of_birth A date of birth.
-	 *     @type string $first_name    A first name.
-	 *     @type string $form_url      URL that the form posted from.
-	 *     @type string $last_name     A last name.
-	 *     @type string $middle_name   A middle name.
-	 *     @type string $no_degree     Optional. A code for non-degree
-	 *                                 selections.
-	 *     @type string $no_fill       A field that needs to be blank for the
-	 *                                 form to validate.
-	 *     @type string $proftech_type Optional. Professional technical
-	 *                                 certificate / degree code.
-	 *     @type string $what_degree   Optional. A non-professional technical
-	 *                                 certificate / degree code.
-	 * }
-	 *
-	 * @return boolean True if the post fields are populated (even if invalid),
-	 *                 false if the post fields are not populated correctly.
 	 */
-	public function save_data( $post_array ) {
-		$data_not_valid                 = false;
-		$honey_pot_field                = 'no_fill';
-		$maximum_first_last_name_length = 32;
-		$maximum_middle_name_length     = 16;
-		$program_of_study = $this->get_program_of_study( $post_array );
-
-		/*
-		 * Check that all required form fields were posted. On failure load a
-		 * blank page because someone is likely tampering with the form.
-		 * Otherwise set initial variables.
-		 */
-		if ( ! empty( $post_array['form_url'] )
-			&& ! empty( $post_array['date_of_birth'] )
-			&& ! empty( $post_array['first_name'] )
-			&& ! empty( $post_array['last_name'] )
-			&& $program_of_study
-			&& ( array_key_exists( $honey_pot_field, $post_array )
-				&& empty( $post_array[ $honey_pot_field ] )
-			)
-		) {
-			$form_url = $post_array['form_url'];
-			$date_of_birth = $post_array['date_of_birth'];
-			$first_name = $post_array['first_name'];
-			$last_name = $post_array['last_name'];
-		} else {
-			$this->model->set_template_uri(
-				'template/blank-page-template.php'
-			);
-			return false;
-		}
-
-		$middle_name = NULL;
-		if ( ! empty( $post_array['middle_name'] ) ) {
-			$middle_name = $post_array['middle_name'];
-		}
-
-		// Validate variables
-		if ( ! $this->is_valid_date_of_birth( $date_of_birth ) ) {
-			$this->model->add_error(
-				'Date of birth is not valid or is in the future.'
-			);
-			$data_not_valid = true;
-		}
-		if ( ! $this->is_valid_name( $first_name,
-					$maximum_first_last_name_length
-				)
-		) {
-			$this->model->add_error(
-				'First name contains invalid characters.'
-			);
-			$data_not_valid = true;
-		}
-		if ( ! $this->is_valid_name( $last_name,
-					$maximum_first_last_name_length
-				)
-		) {
-			$this->model->add_error(
-				'Last name contains invalid characters.'
-			);
-			$data_not_valid = true;
-		}
-		if ( ! $this->is_valid_program_of_study( $program_of_study ) ) {
-			$this->model->add_error(
-				'The program of study code is invalid.'
-			);
-		}
-		if ( isset( $middle_name )
-			&& ! $this->is_valid_name( $middle_name,
-					$maximum_middle_name_length
-				)
-		) {
-			$this->model->add_error(
-				'Middle name contains invalid characters.'
-			);
-			$data_not_valid = true;
-		}
-
-		// If variables are not valid
-		if ( $data_not_valid ) {
-			$this->model->set_redirect_url( $form_url );
-			$this->model->set_template_uri(
-				'template/error-redirect-template.php'
-			);
-			return true;
-		}
-
-		// Set model variables
-		$this->model->set_student_date_of_birth( $date_of_birth );
-		$this->model->set_student_first_name( $first_name );
-		$this->model->set_student_last_name( $last_name );
-		$this->model->set_student_middle_name( $middle_name );
-		$this->model->set_program_of_study( $program_of_study );
-
-		// Have model save the data
-		$this->model->save_data();
-
-		// Set the model signature
+	public function save_variables() {
+		parent::save_variables();
+		$this->model->set_program_of_study(
+			$this->get_program_of_study_code()
+		);
+		$this->model->save_data_and_set_reference_number();
 		$this->model->set_signature();
-
-		return true;
 	}
 }
